453482d8734767694c2aea6470a15138b966652a
Dmitry Baranovskiy <dmitry@baranovskiy.com>
2013-10-18 15:38:27 +1100

    Fix for transform strings to be equal to what you set them to be (Matrix roundtrip distortion fix)

--- dist/snap.svg.js
+++ dist/snap.svg.js
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // 
-// build: 2013-10-15
+// build: 2013-10-18
 // Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -1511,7 +1511,6 @@ function Matrix(a, b, c, d, e, f) {
         this.f = out[1][2];
         return this;
     };
-// SIERRA Matrix.invert(): Unclear what it means to invert a matrix.
     /*\
      * Matrix.invert
      [ method ]
@@ -1545,7 +1544,6 @@ function Matrix(a, b, c, d, e, f) {
     matrixproto.translate = function (x, y) {
         return this.add(1, 0, 0, 1, x, y);
     };
-    // SIERRA: do cx/cy default to the center point, as in CSS? If so, Snap appears to resolve important discrepancies between how transforms behave in SVG & CSS.
     /*\
      * Matrix.scale
      [ method ]
@@ -1555,6 +1553,7 @@ function Matrix(a, b, c, d, e, f) {
      - y (number) #optional amount to scale along the vertical axis. (Otherwise `x` applies to both axes.)
      - cx (number) #optional horizontal origin point from which to scale
      - cy (number) #optional vertical origin point from which to scale
+     * Default cx, cy is the middle point of the element.
     \*/
     matrixproto.scale = function (x, y, cx, cy) {
         y == null && (y = x);
@@ -2332,31 +2331,20 @@ function extractTransform(el, tstr) {
             return new Matrix;
         }
         tstr = svgTransform2string(tstr);
-    } else if (!rgTransform.test(tstr)) {
-        tstr = svgTransform2string(tstr);
     } else {
-        tstr = Str(tstr).replace(/\.{3}|\u2026/g, el._.transform || E);
+        el._.transform = tstr;
+        if (!rgTransform.test(tstr)) {
+            tstr = svgTransform2string(tstr);
+        } else {
+            tstr = Str(tstr).replace(/\.{3}|\u2026/g, el._.transform || E);
+        }
     }
-    el._.transform = tstr;
     var m = transform2matrix(tstr, el.getBBox(1));
     if (doReturn) {
         return m;
     } else {
         el.matrix = m;
     }
-
-    // _.sx = sx;
-    // _.sy = sy;
-    // _.deg = deg;
-    // _.dx = dx = m.e;
-    // _.dy = dy = m.f;
-    // 
-    // if (sx == 1 && sy == 1 && !deg && _.bbox) {
-    //     _.bbox.x += +dx;
-    //     _.bbox.y += +dy;
-    // } else {
-    //     _.dirtyT = 1;
-    // }
 }
 Snap._unit2px = unit2px;
 function getSomeDefs(el) {
@@ -2652,7 +2640,7 @@ function arrayFirstValue(arr) {
         return Snap._.box(_.bbox);
     };
     var propString = function () {
-        return this.local;
+        return this.string;
     };
 // SIERRA Element.transform(): seems to allow two return values, one of which (_Element_) is undefined.
 // SIERRA Element.transform(): if this only accepts one argument, it's unclear how it can both _get_ and _set_ a transform.
@@ -2681,14 +2669,17 @@ function arrayFirstValue(arr) {
         var _ = this._;
         if (tstr == null) {
             var global = new Matrix(this.node.getCTM()),
-                local = extractTransform(this);
+                local = extractTransform(this),
+                localString = local.toTransformString(),
+                string = Str(local) == Str(this.matrix) ?
+                            _.transform : localString;
             return {
-                string: Str(_.transform) || "",
+                string: string,
                 globalMatrix: global,
                 localMatrix: local,
                 diffMatrix: global.clone().add(local.invert()),
                 global: global.toTransformString(),
-                local: local.toTransformString(),
+                local: localString,
                 toString: propString
             };
         }
@@ -3168,10 +3159,6 @@ function arrayFirstValue(arr) {
         }
         return res;
     };
-    // SIERRA unfamiliar with the phrase _caring function,_ so the text for the _setter_ param isn't clear.
-    // SIERRA With the animation's start/end states defined, how is its _speed_ distinguished from its _duration_?
-    // SIERRA Text explaining the mina format should move to the section on the mina object interface. (Prior comment applies: object interfaces need to also be documented.)
-    // SIERRA unclear how to express a custom _easing_ (+)
     /*\
      * Snap.animate
      [ method ]
@@ -3193,6 +3180,14 @@ function arrayFirstValue(arr) {
      o     status (function) gets or sets the status of the animation,
      o     stop (function) stops the animation
      o }
+     | var rect = Snap().rect(0, 0, 10, 10);
+     | Snap.animate(0, 10, function (val) {
+     |     rect.attr({
+     |         x: val
+     |     });
+     | }, 1000);
+     | // in given context is equivalent to
+     | rect.animate({x: 10}, 1000);
     \*/
     Snap.animate = function (from, to, setter, ms, easing, callback) {
         if (typeof easing == "function" && !easing.length) {
@@ -3204,7 +3199,6 @@ function arrayFirstValue(arr) {
         callback && eve.once("mina.finish." + anim.id, callback);
         return anim;
     };
-    // SIERRA Element.stop(). Does it _stop_ or _pause_ the animations? If you run Element.animate() to restart the animation, does it commence from the beginning?
     /*\
      * Element.stop
      [ method ]
