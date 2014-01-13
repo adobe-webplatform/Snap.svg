1b9eb0ea3cb8fdb547782171da6120c3cd68df63
Sam Breed <sam@quickleft.com>
2014-01-12 19:44:30 -0700

    explicitly pass the global scope to the SVG iffe

--- src/svg.js
+++ src/svg.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var Snap = (function() {
+var Snap = (function(root) {
 Snap.version = "0.2.0";
 /*\
  * Snap
@@ -50,8 +50,8 @@ Snap.toString = function () {
 };
 Snap._ = {};
 var glob = {
-    win: window,
-    doc: window.document
+    win: root.window,
+    doc: root.window.document
 };
 Snap._.glob = glob;
 var has = "hasOwnProperty",
@@ -3643,4 +3643,4 @@ Snap.plugin = function (f) {
 };
 glob.win.Snap = Snap;
 return Snap;
-}());
+}(window || this));
