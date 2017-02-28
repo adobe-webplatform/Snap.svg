ef37065a41681330028b5a1bc95f03701c5e213a
Chris Trevino <darthtrevino@gmail.com>
2017-02-27 18:46:25 -0800

    Remove cjs banner and footer files, correct package.json main field

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,13 @@ var path = require('path');
 module.exports = {
     entry: "./test/webpackentry.js",
     output: {
-        filename: "./.tmp/webpacked.js",
+        filename: "./.tmp/test-bundle.js",
+    },
+    devServer: {
+        contentBase: ".",
+        historyApiFallback: {
+            index: "test/test.html",
+        },
     },
     resolve: {
         alias: {
