27374689c9671a0836ab00f22a6079f177616501
Chris Trevino <darthtrevino@gmail.com>
2017-02-27 18:48:43 -0800

    Include all of the test suites in the webpack entry file

--- test/webpackentry.js
+++ test/webpackentry.js
@@ -1,2 +1,16 @@
-var snapsvg = require('snapsvg');
-console.log("SNAP IS", snap);
+var snapsvg = require("snapsvg");
+require("./system");
+require("./paper");
+require("./snap-tests");
+require("./class");
+require("./primitives");
+require("./colors");
+require("./attrs");
+require("./attradd");
+require("./path");
+require("./element");
+require("./gradients");
+require("./set");
+require("./filter");
+require("./matrix");
+require("./mina");
